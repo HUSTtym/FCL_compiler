38f8d33506a621e77c123a2c2bffcea8774aab4a
HUSTtym <865486907@qq.com>
2022-07-28 21:48:10 +0800

    备注内容

---
+++ create_test/00_float_params*.c
@@ -0,0 +1,42 @@
+
+float p1 = 1, p2[10] = {1}, p3[2][3] = {1}, p4 = 1, p5 = 1, p6 = 1, p7 = 1, p8 = 1, p9 = 1,
+      p11 = 1, p12[10] = {1}, p13[2][3] = {1}, p14 = 1, p15 = 1, p16 = 1, p17 = 1, p18 = 1, p19 = 1,
+      p21 = 1, p22[10] = {1}, p23[2][3] = {1}, p24 = 1, p25 = 1, p26 = 1, p27 = 1, p28 = 1, p29 = 1,
+      p31 = 1, p32[10] = {1}, p33[2][3] = {1}, p34 = 1, p35 = 1, p36 = 1, p37 = 1, p38 = 1, p39 = 1,
+      p41 = 1, p42[10] = {1}, p43[2][3] = {1}, p44 = 1, p45 = 1, p46 = 1, p47 = 1, p48 = 1, p49 = 1;
+
+float func(float p1, float p2[], float p3[][3], float p4, float p5, float p6, float p7, float p8, float p9, float p11, float p12[], float p13[][3], float p14, float p15, float p16, float p17, float p18, float p19, float p21, float p22[], float p23[][3], float p24, float p25, float p26, float p27, float p28, float p29, float p31, float p32[], float p33[][3], float p34, float p35, float p36, float p37, float p38, float p39, float p41, float p42[], float p43[][3], float p44, float p45, float p46, float p47, float p48, float p49, float p51, float p52[], float p53[][3], float p54, float p55, float p56, float p57, float p58, float p59, float p61, float p62[], float p63[][3], float p64, float p65, float p66, float p67, float p68, float p69, float p71, float p72[], float p73[][3], float p74, float p75, float p76, float p77, float p78, float p79, float p81, float p82[], float p83[][3], float p84, float p85, float p86, float p87, float p88, float p89, float p91, float p92[], float p93[][3], float p94, float p95, float p96, float p97, float p98, float p99)
+{
+    return p1 + p2[0] + p3[0][0] + p4 + p5 + p6 + p7 + p8 + p9 +
+           p11 + p12[0] + p13[0][0] + p14 + p15 + p16 + p17 + p18 + p19 +
+           p21 + p22[0] + p23[0][0] + p24 + p25 + p26 + p27 + p28 + p29 +
+           p31 + p32[0] + p33[0][0] + p34 + p35 + p36 + p37 + p38 + p39 +
+           p41 + p42[0] + p43[0][0] + p44 + p45 + p46 + p47 + p48 + p49 +
+           p51 + p52[0] + p53[0][0] + p54 + p55 + p56 + p57 + p58 + p59 +
+           p61 + p62[0] + p63[0][0] + p64 + p65 + p66 + p67 + p68 + p69 +
+           p71 + p72[0] + p73[0][0] + p74 + p75 + p76 + p77 + p78 + p79 +
+           p81 + p82[0] + p83[0][0] + p84 + p85 + p86 + p87 + p88 + p89 +
+           p91 + p92[0] + p93[0][0] + p94 + p95 + p96 + p97 + p98 + p99;
+}
+
+int main()
+{
+
+    float p51 = 1, p52[10] = {1}, p53[2][3] = {1}, p54 = 1, p55 = 1, p56 = 1, p57 = 1, p58 = 1, p59 = 1,
+          p61 = 1, p62[10] = {1}, p63[2][3] = {1}, p64 = 1, p65 = 1, p66 = 1, p67 = 1, p68 = 1, p69 = 1,
+          p71 = 1, p72[10] = {1}, p73[2][3] = {1}, p74 = 1, p75 = 1, p76 = 1, p77 = 1, p78 = 1, p79 = 1,
+          p81 = 1, p82[10] = {1}, p83[2][3] = {1}, p84 = 1, p85 = 1, p86 = 1, p87 = 1, p88 = 1, p89 = 1,
+          p91 = 1, p92[10] = {1}, p93[2][3] = {1}, p94 = 1, p95 = 1, p96 = 1, p97 = 1, p98 = 1, p99 = 1;
+    putfloat(func(
+        p1, p2, p3, p4, p5, p6, p7, p8, p9,
+        p11, p12, p13, p14, p15, p16, p17, p18, p19,
+        p21, p22, p23, p24, p25, p26, p27, p28, p29,
+        p31, p32, p33, p34, p35, p36, p37, p38, p39,
+        p41, p42, p43, p44, p45, p46, p47, p48, p49,
+        p51, p52, p53, p54, p55, p56, p57, p58, p59,
+        p61, p62, p63, p64, p65, p66, p67, p68, p69,
+        p71, p72, p73, p74, p75, p76, p77, p78, p79,
+        p81, p82, p83, p84, p85, p86, p87, p88, p89,
+        p91, p92, p93, p94, p95, p96, p97, p98, p99));
+    return 0;
+}
---
+++ create_test/01_multi_branch.c
@@ -0,0 +1,519 @@
+int main()
+{
+    int a = 3 - 4 + 1;
+    if (a > 1)
+    {
+        int v1;
+        v1 = 23;
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+    else if (a > 1)
+    {
+    }
+}
